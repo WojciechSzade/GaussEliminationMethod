0c011bc52a70fc92402881d647a15eaa775bcf4d
Damian Denc <ddenc@leonsoftware.com>
2021-12-16 18:53:46 +0100

    swapping rows and add test10

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,4 +1,5 @@
 #include "backsubst.h"
+
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
@@ -8,25 +9,17 @@ int backsubst(Matrix *x, Matrix *mat, Matrix *b)
 {
     if (mat->r != mat->c) return 2;
     double sum = 0;
-    //x->data[x->r][0] = b->data[b->r][0]/mat->data[mat->r][mat->r];
     for (int i = 0; i < x->r; i++)
     {
         sum = b->data[b->r - i - 1][0];
         for (int j = 0; j < i; j++)
         {
-            sum -= mat->data[x->r - i - 1][mat->r - j - 1] * x->data[x->r - j - 1][0]; //możliwe ze przy data na odwrot []
+            sum -= mat->data[x->r - i - 1][mat->r - j - 1] * x->data[x->r - j - 1][0];
         }
         if (mat->data[x->r - i - 1][x->r - i - 1] != 0) x->data[x->r - i - 1][0] = sum/mat->data[x->r - i - 1][x->r - i - 1];
         else return 1;
     }
 
-
-    /*int i;
-    for (i =0; i < x->r; i++) {
-        x->data[i][0] = b->data[i][0];
-
-    }
-    */
     return 0;
 
 }
--- src/gauss.c
+++ src/gauss.c
@@ -4,19 +4,48 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
+int select_max_row(Matrix *mat, int column) {
+    double mx = -DBL_MAX;
+    int row = column;
+    for (int i=column; i<mat->r; ++i) {
+		if (mat->data[i][column]>mx) {
+			row = i;
+			mx = mat->data[i][column];
+		} else if (-mat->data[i][column]>mx) {
+			row = i;
+			mx = -mat->data[i][column];
+		}
+    }
+    return row;
+}
+
+void swap_rows(Matrix *mat, int row, Matrix *b) {
+	int row_to_swap = select_max_row(mat, row);
+	double tmp;
+	for (int i=0; i<mat->r; ++i) {
+		tmp = mat->data[row][i];
+		mat->data[row][i]=mat->data[row_to_swap][i];
+		mat->data[row_to_swap][i]=tmp;
+	}
+	tmp = b->data[row][0];
+	b->data[row][0]=b->data[row_to_swap][0];
+	b->data[row_to_swap][0]=tmp;
+}
+
 int eliminate(Matrix *mat, Matrix *b) {
     int rows=mat->r, columns=mat->c;
     double divider;
 
     for (int k=0; k<columns; ++k) {
-		// tu bedzie funkcja do wyboru elementu glownego 
-		if (mat->data[k][k] == 0) {
-			return 1;
+        swap_rows(mat, k, b);
+        if (mat->data[k][k] == 0) {
+            return 1;
         }
         for (int i=k+1; i<rows; ++i) {
             divider=mat->data[i][k]/mat->data[k][k];
             for (int j=0; j<columns; ++j) {
-				mat->data[i][j]-=divider*mat->data[k][j];
+                mat->data[i][j]-=divider*mat->data[k][j];
             }
             b->data[i][0]-=divider*b->data[k][0];
         }
